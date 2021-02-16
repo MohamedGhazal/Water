f78b21bfa41a3481c6c36ff2e34ea27888c8c0cc
Mo <mohamedghazal1@hotmail.com>
2021-02-15 21:09:13 -0500

    Initial Commit

---
+++ Shared/WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterApp.swift
+//  Shared
+//
+//  Created by Mohamed Ghazal on 2021-02-15.
+//
+
+import SwiftUI
+
+@main
+struct WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
